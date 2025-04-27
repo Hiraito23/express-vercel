a2b6aa36bc5186ccdf0dfbef5cbe058d46539f67
Hiraito23 <Malvin.hiraito23@gmail.com>
2025-04-27 11:12:17 +0800

    Updated

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
 // Ambil data dari .env
-const supabaseUrl = "https://aiqixyyhlwfvnblgrttu.supabase.co"
-const supabaseKey = "7bXaDDMD+DvoyHmz5s4fgTYs1j5HjplAFjDiu3aRmRmIXMf7ipTLrwxVSSHi6EDoymIooT/JugTafjMpxaanAw=="
+const supabaseUrl = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpcWl4eXlobHdmdm5ibGdydHR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3MjE3MzIsImV4cCI6MjA2MTI5NzczMn0.Nejmvm0-eo6SyinW6fyujwzP_bDSSI71tP3ZNvOLDTg"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpcWl4eXlobHdmdm5ibGdydHR1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NTcyMTczMiwiZXhwIjoyMDYxMjk3NzMyfQ.IuvqPhCVBaH5W1QOXAgy-pIxhMQCQFpIjKgq1SYy_x0"
 
 // Create Supabase Client
 const supabase = createClient(supabaseUrl, supabaseKey);
